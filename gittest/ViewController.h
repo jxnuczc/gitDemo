02fb3fa917ef65508405f6ac5627712d19688133
陈志超 <841178964@qq.com>
2016-05-31 08:33:28 +0800

    Initial Commit

---
+++ gittest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gittest
+//
+//  Created by chenzhichao on 16/5/31.
+//  Copyright © 2016年 九个太阳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ gittest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  gittest
+//
+//  Created by chenzhichao on 16/5/31.
+//  Copyright © 2016年 九个太阳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
